894b64d7e5a81ed2da371be8c0688a5f020b030c
konsin1988 <konsin1988@gmail.com>
2024-02-26 10:16:14 +0300

    Adding random starting card

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
-const cards = document.querySelectorAll(".card");
+function activateCards(active) {
+  const cards = document.querySelectorAll(".card");
 
-for (const card of cards) {
-  card.addEventListener("click", () => {
-    clearCards(cards);
-    card.classList.add("active");
-  });
+  cards[active].classList.add("active");
+
+  for (const card of cards) {
+    card.addEventListener("click", () => {
+      clearCards(cards);
+      card.classList.add("active");
+    });
+  }
+
+  function clearCards(cards) {
+    cards.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
 }
 
-function clearCards(cards) {
-  cards.forEach((slide) => {
-    slide.classList.remove("active");
-  });
-}
+activateCards(Math.floor(Math.random() * 5));
